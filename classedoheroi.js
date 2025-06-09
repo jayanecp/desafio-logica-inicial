9dfa5354f2c42f0a15569bdd441d8b259d778332
jayanecp <jayanepereira68@gmail.com>
2025-06-09 20:22:46 -0300

    Add files via upload

---
+++ classedoheroi.js
@@ -0,0 +1,31 @@
+//delarando a variável para armazenar o nome e a quantidade de xp
+let nameHeroi = "Lucius"
+let quantiaXp = 3150
+let nivel = " ";
+if (quantiaXp < 1000 ){
+    nivel = "ferro"
+    
+}
+else if (quantiaXp >= 1001 && quantiaXp <= 2000) {
+    nivel = "Bronze"
+}
+else if (quantiaXp >= 2001 && quantiaXp <=5000){
+   nivel = "Prata"
+}
+else if (quantiaXp >= 5001 && quantiaXp <= 7000) {
+    nivel = "Ouro"
+}
+else if (quantiaXp >= 7001 && quantiaXp <= 8000){
+    nivel = "Platina"
+}
+else if (quantiaXp >= 8001 && quantiaXp <= 9000){
+    nivel = "Ascendente"
+}
+else if (quantiaXp >= 9001 && quantiaXp <= 10000){
+   nivel = "Imortal"
+}
+else 
+   nivel = "Radiante"
+
+//Saída
+    console.log ( " O herói "  + nameHeroi  +  " está no nível "  +  nivel  )
